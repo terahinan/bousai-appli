f7131b515f4fc087d0a93035b074348e61709cdf
terahinan <akira-teramura@iris.eonet.ne.jp>
2026-08-01 12:29:27 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "bousai-earthquake-v2";
+const FILES = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./icon.svg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
